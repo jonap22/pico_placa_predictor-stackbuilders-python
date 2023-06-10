0a2dc09c48ffc532954c0a99f79c07c4a2d58374
Jonathan Puglla <jonathan.puglla@outlook.com>
2023-06-09 19:51:42 -0500

    fix: Initialize project repository
    This initializes the project repository and sets up the basic project structure by adding the necessary files and folders.

---
+++ main/pico_placa_predictor.py
@@ -0,0 +1,31 @@
+# author: Jonathan Puglla
+# date: June 9th, 2023
+
+import re
+from datetime import datetime, time
+
+
+class PicoPlacaPredictor:
+    def __init__(self, rules_file_path):
+        # TODO: add functionality
+        return
+
+    def load_rules(self, file_path):
+        # TODO: add functionality
+        return
+
+    def is_plate_restricted(self, plate_number, date, time):
+        # TODO: add functionality
+        return
+
+    def validate_date(self, date):
+        # TODO: add functionality
+        return
+
+    def validate_time(self, time_str):
+        # TODO: add functionality
+        return
+
+    def is_restricted_time(self, time_obj):
+        # TODO: add functionality
+        return
