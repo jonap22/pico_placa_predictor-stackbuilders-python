97ccca004a2ef524e1fa7b5fb2bb31ce81ca02e0
Jonathan Puglla <jonathan.puglla@outlook.com>
2023-06-09 20:20:47 -0500

    feat: Implemented validate_plate_number function
    This implements the function to validate the format of the licence plate number by matching the license plate number with the XXX-1234 format.

--- main/pico_placa_predictor.py
+++ main/pico_placa_predictor.py
@@ -6,7 +6,14 @@ from datetime import datetime, time
 
 
 def load_rules(file_path):
+    """
+    Loads the Pico y Placa rules from a file.
+
+    :param file_path:
+    :return: rules - a dictionary with the Pico y Placa rules
+    """
     rules = {}
+
     with open(file_path, 'r') as rules_file:
         for line in rules_file:
             plate_number, allowed_days = line.strip().split(':')
@@ -19,6 +26,15 @@ class PicoPlacaPredictor:
     def __init__(self, rules_file_path):
         self.rules = load_rules(rules_file_path)
         return
+
+    def validate_plate_number(self, plate_number):
+        """
+        Validates the format (XXX-1234) of the license plate number.
+        :param plate_number:
+        :return: boolean
+        """
+        pattern = r'^[A-Z]{3}-\d{4}$'
+        return re.match(pattern, plate_number) is not None
 
     def is_plate_restricted(self, plate_number, date, time_str):
         # TODO: add functionality
